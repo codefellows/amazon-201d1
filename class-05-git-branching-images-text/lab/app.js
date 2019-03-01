12980371e24c983a72c9d1615d30ce96968c5419
adireddy999 <adireddyvinaykumar@gmail.com>
2019-03-01 22:06:48 +0530

    Problem4 solved

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -77,12 +77,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var sum1 = sum(sumArr[0], sumArr[1])[0];
+var result = sum(sum1,sumArr[2])[0];
+var array = [result, sumArr[0]+","+sumArr[1]+","+sumArr[2]+" was passed in as an array of numbers, and "+result+" is their sum."];
+return array;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
