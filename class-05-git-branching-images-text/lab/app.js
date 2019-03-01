9e4dc65404fbe94b25721f6b54b3fb683e7c17dd
Ananthapadmanabhan <kaananth@amazon.com>
2019-02-28 23:19:22 -0800

    Updated function multiplyAnyArray(multArr) with required logic

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -145,10 +145,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+    var product = 1;
+    var message = null;
+    
+    for(var i=0; i<dynamicArray.length; i++){
+        product = multiply(product, dynamicArray[i])[0];
+        message = message + dynamicArray[i] + ",";
+    }
+    message = 'The numbers ' + message.substr(0,message.length - 1) + ' have a product of ' + product+ '.';
+    return ([product,message]);
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
