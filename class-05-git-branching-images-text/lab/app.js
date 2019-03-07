48f280eab237ad354b9512d25ed40e3e27f8c1d5
Prashant Suri <suripr@amazon.com>
2019-03-07 18:49:58 +0530

    Multiplication of Array Solved

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -101,11 +101,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+var multiplyArr = multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0];
+var result = [multiplyArr, "The numbers " + multArr.toString() + " have a product of " + multiplyArr + "."];
+return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
